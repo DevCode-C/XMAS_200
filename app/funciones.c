9d4b9396bc14fd6e502ef80420264cd9dd5e2fd3
DevCode-C <emmanuelparedescamargo@gmail.com>
2021-06-08 03:01:58 -0500

    Secuency ended

--- app/funciones.c
+++ app/funciones.c
@@ -6,5 +6,66 @@
 #include "stm32f0xx_hal_rcc.h"
 #include "funciones.h"
 
+void blinky(void)
+{
+    HAL_GPIO_TogglePin(GPIOC,0xff);
+}
 
+void altern(void)
+{
+    static uint8_t pin=0;
+    pin ^=1; 
+    HAL_GPIO_WritePin(GPIOC,0xAA,pin);
+    HAL_GPIO_WritePin(GPIOC,~(0xAA),!pin);
+}
+
+void rolling(void)
+{
+    static uint8_t i = 128;
+    HAL_GPIO_WritePin(GPIOC,i,SET);
+    HAL_GPIO_WritePin(GPIOC,~(i),RESET);
+    i >>= 1;
+    if (i == 0)
+    {
+        i =128;
+    }
+    
+
+}
+
+void kit(void)
+{
+    const uint8_t sec[8] = {0x81,0x42,0x24,0x18,0x24,0x42,0x81};
+    static uint8_t i = 0;
+    HAL_GPIO_WritePin(GPIOC,sec[i],SET);
+    HAL_GPIO_WritePin(GPIOC,~(sec[i]),RESET);
+    i++;
+    if (i>=7)
+    {
+        i=0;
+    }
+    
+}
+
+void car(void)
+{
+    static uint8_t i = 0;
+
+    HAL_GPIO_WritePin(GPIOC,0xC3,SET);
+    HAL_GPIO_WritePin(GPIOC,~(0xC3),RESET);
+    if (i>1)
+    {
+        
+        HAL_GPIO_WritePin(GPIOC,0x18,SET);
+        HAL_GPIO_WritePin(GPIOC,~(0x18),RESET);
+        i=0;
+    }
+    i++;
+}
+
+void none(void)
+{
+    HAL_GPIO_WritePin(GPIOC,0xFF,RESET);
+    
+}
 
--- app/main.c
+++ app/main.c
@@ -13,12 +13,34 @@
 Brief.- Punto de entrada del programa
 -------------------------------------------------------------------------------------------------*/
 
+#define strlen_p(x) strlen((const char*)x)
+
+
+const uint8_t* msgError     =(uint8_t*)"ERROR\n";
+const uint8_t* msgOk        =(uint8_t*)"OK\n";
+const uint8_t* Comando      =(uint8_t*)"XMAS=";
+
+const uint8_t* secuencias[7] = {(uint8_t*)"blinky,",
+                                (uint8_t*)"altern,",
+                                (uint8_t*)"rolling,",
+                                (uint8_t*)"kit,",
+                                (uint8_t*)"car,",
+                                (uint8_t*)"none,"
+};
+
+void (*prtFunctions[7])(void) = {blinky,altern,rolling,kit,car,none};
+
 UART_HandleTypeDef UartHandle;
 __IO ITStatus uartState = RESET;
 __IO ITStatus status = RESET;
+__IO HAL_StatusTypeDef flag_String = HAL_BUSY;
+
+
 uint8_t RxByte;
 uint8_t RxBuffer[20];
-int16_t timedelay= 100;
+uint32_t tickTimer; 
+uint16_t timer = 100;
+uint8_t function_list = 5;
 
 void UART_Init(void);
 
@@ -26,26 +48,80 @@ int main( void )
 {
     HAL_Init( );
     UART_Init();
-
+    tickTimer = HAL_GetTick();
     for (; ;)
     {
-        if (status == SET)
+
+        if (status == SET )
         {
             status = RESET;
-            timedelay = atoi((const char *)RxBuffer);
-            if ((timedelay > 999 || timedelay < 10))
+            
+            if (!memcmp(RxBuffer,Comando,strlen_p(Comando)))
             {
-                timedelay= 100;
-                HAL_UART_Transmit_IT(&UartHandle,(uint8_t*)"Numero no valido\r\n",sizeof("Numero no valido\r\n")-1);
+                memcpy(RxBuffer,&RxBuffer[strlen_p(Comando)],(sizeof(RxBuffer) - sizeof(Comando)));
+
+                for (uint8_t i = 0; i < 7; i++)
+                {
+                    if (!memcmp(RxBuffer,secuencias[i],strlen_p(secuencias[i])))
+                    {
+                        flag_String = HAL_OK;
+                        memcpy(RxBuffer,&RxBuffer[strlen_p(secuencias[i])],(sizeof(RxBuffer)-sizeof(secuencias[i])));
+                        function_list = i;
+                        break;
+                    }  
+                    else
+                    {
+                        flag_String = HAL_ERROR;
+                    }
+                    
+                }
+                if (flag_String == HAL_OK)
+                {
+                    HAL_UART_Transmit_IT(&UartHandle,(uint8_t*)RxBuffer,strlen_p(RxBuffer));
+                }
+                else
+                {
+                    HAL_UART_Transmit_IT(&UartHandle,(uint8_t*)msgError,strlen_p(msgError));
+                }
+                
+                
             }
             else
             {
-                HAL_UART_Transmit_IT(&UartHandle,(uint8_t*)"Numero valido\r\n",sizeof("Numero valido\r\n")-1);
+                HAL_UART_Transmit_IT(&UartHandle,(uint8_t*)msgError,strlen_p(msgError));
             }
             
+            
+            
         }
-        HAL_GPIO_TogglePin(GPIOC,GPIO_PIN_5);
-        HAL_Delay(timedelay);
+
+        if (HAL_GetTick() - tickTimer > timer)
+        {
+            tickTimer = HAL_GetTick();
+            HAL_GPIO_TogglePin(GPIOA,GPIO_PIN_5);
+            switch (function_list)
+            {
+                case 0:
+                    blinky();
+                    break;
+                case 1:
+                    altern();
+                    break;
+                case 2:
+                    rolling();
+                    break;
+                case 3:
+                    kit();
+                    break;
+                case 4:
+                    car();
+                    break;
+                case 5:
+                    none();
+                    break;
+            }
+        }
+        
         
     } 
     return 0u;
@@ -55,7 +131,7 @@ int main( void )
 void UART_Init()
 {
     UartHandle.Instance             = USART2;
-    UartHandle.Init.BaudRate        = 9600;
+    UartHandle.Init.BaudRate        = 115200;
     UartHandle.Init.WordLength      = UART_WORDLENGTH_8B;
     UartHandle.Init.StopBits        = UART_STOPBITS_1;
     UartHandle.Init.Parity          = UART_PARITY_NONE;
